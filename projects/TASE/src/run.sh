2961d173d12600d2d04cf4918cf39ec9a222bca9
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2012-10-26 19:21:39 +0000

    tase.log

--- projects/TASE/src/run.sh
+++ projects/TASE/src/run.sh
@@ -1,10 +1,10 @@
 #/bin/sh
 
+rm tase.log
+
 sh import_currency.sh
 
 ./scrapy-ctl.py crawl portfolio
-
-#./scrapy-ctl.py crawl markets stocks bonds funds weather
 
 ./scrapy-ctl.py crawl markets
 ./scrapy-ctl.py crawl weather
@@ -12,6 +12,7 @@ sh import_currency.sh
 ./scrapy-ctl.py crawl funds
 ./scrapy-ctl.py crawl bonds
 
+#./scrapy-ctl.py crawl markets stocks bonds funds weather
 
 #./scrapy-ctl.py crawl stocks & tail -f scrapy.log
 #scrapy crawl stocks --set FEED_URI=items.json --set FEED_FORMAT=json
