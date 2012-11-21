4831fe301b5a7808f6bf308cb04c6e5029bada54
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2012-11-21 08:10:43 +0000

    geomodel

--- projects/marina/appengine/marinas/models.py
+++ projects/marina/appengine/marinas/models.py
@@ -2,6 +2,7 @@ from google.appengine.ext import db
 #from google.appengine.tools import bulkloader
 from google.appengine.ext import bulkload
 from google.appengine.api import datastore_types
+#from geo.geomodel import GeoModel
 
 class Marina(db.Model):
 	name = db.StringProperty()
@@ -9,6 +10,13 @@ class Marina(db.Model):
 	longitude = db.FloatProperty()
 	#point = db.GeoPtProperty()
 	marina_type = db.IntegerProperty()
+
+#class Marina2(GeoModel):
+#	name = db.StringProperty()
+#	latitude = db.FloatProperty()
+#	longitude = db.FloatProperty()
+#	#point = db.GeoPtProperty()
+#	marina_type = db.IntegerProperty()
 
 class MarinaLoader(bulkload.Loader):
 	def __init__(self):
