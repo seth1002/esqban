87e28330ab6ffaeb23e7128207702f4006fc191c
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-01-14 21:16:39 +0000

    updated currency import

--- projects/TASE/src/db/import_currency.sh
+++ projects/TASE/src/db/import_currency.sh
@@ -1,22 +1,22 @@
 #/bin/sh
 
 #wget -N 'http://www.bankisrael.gov.il/deptdata/mth/yazig.zip'
-wget -N 'http://www.boi.org.il/he/Markets/Documents/yazig.zip'
+wget -N 'http://www.boi.org.il/he/Markets/Documents/yazigmizt.xls'
 
-unzip -o yazig.zip
+#unzip -o yazig.zip
 
-mv YAZIG.XLS yazig.xls
+#mv YAZIG.XLS yazig.xls
 
 #xls2csv yazig.xls > yazig.csv
 #xls2csv -x yazig.xls -f%d/%m/%Y > yazig.csv
-xls2csv -x yazig.xls -c yazig.csv
+xls2csv -x yazigmizt.xls -c yazig.csv
 
 #mysql -usqba -p12345 tase < import_currency.sql
 mysql -utase -p12345 --local-infile tase < import_currency.sql
 
-rm yazig.zip
+#rm yazig.zip
 rm yazig.csv
-rm yazig.xls
+rm yazigmizt.xls
 
 
 
