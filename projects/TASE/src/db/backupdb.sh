5861e361a78a87ffac6f63fdceaca3573acb6bff
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-03-13 20:54:43 +0000

    startvm

--- projects/TASE/src/db/backupdb.sh
+++ projects/TASE/src/db/backupdb.sh
@@ -5,6 +5,6 @@ LOGFILE="tase_backup_$NOW.sql.gz"
 
 echo 'Backing up:' $LOGFILE
 
-#mysqldump -uroot -p tase | pv | gzip -9 > $LOGFILE
-mysqldump -uroot -p tase | gzip -9 > $LOGFILE
+#mysqldump -h 192.168.2.106 -uroot -p tase | pv | gzip -9 > $LOGFILE
+mysqldump -h 192.168.2.106 -uroot -p tase | gzip -9 > $LOGFILE
 
--- projects/TASE/startvm.sh
+++ projects/TASE/startvm.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 VBoxManage startvm --type headless "TASE"
 #VBoxHeadless --startvm TASE
-
+#VBoxManage startvm "TASE" -type vrdp
