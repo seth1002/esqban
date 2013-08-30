f21a72168cfa0e873ad78fb62e1d31e2ab25b96e
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-08-30 14:40:16 +0000

    php mysql test

--- projects/TASE/src/www/index.php
+++ projects/TASE/src/www/index.php
@@ -1,6 +1,4 @@
 <html><body><h1>TASE</h1>
-<p>This is the default web page for this server.</p>
-<p>The web server software is running but no content has been added, yet.</p>
 <?php
 $con=mysqli_connect("localhost","sqba","crl2688","tase");
 
