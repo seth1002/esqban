cc0051f68c1457eb7cde11e33f3d8e0fef623ba0
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-08-30 15:58:38 +0000

    chart test 1

---
+++ projects/TASE/src/www/prices.php
@@ -0,0 +1,30 @@
+<?php
+	print $_GET["callback"] . "(" . "[";
+	$user_name = "sqba";
+	$password = "crl2688";
+	$database = "tase";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle);
+
+	if ($db_found) {
+
+	$SQL = "SELECT * FROM company_prices where symbol='" . $_GET["symbol"] . "'";
+	$result = mysql_query($SQL);
+
+	while ( $db_field = mysql_fetch_assoc($result) ) {
+		print "[" . $db_field['_date'] . ", " + $db_field['closing_price'] . "],";
+	}
+
+	mysql_close($db_handle);
+}
+else {
+
+print "Database NOT Found ";
+mysql_close($db_handle);
+
+print "]);";
+
+}
+?>
