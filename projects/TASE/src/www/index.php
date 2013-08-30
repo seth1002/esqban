cbf58111b27d7b0a35b2150ae2a92a922299229d
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-08-30 14:44:06 +0000

    php mysql test

--- projects/TASE/src/www/index.php
+++ projects/TASE/src/www/index.php
@@ -1,18 +1,20 @@
 <html><body><h1>TASE</h1>
 <?php
-$con=mysqli_connect("localhost","sqba","crl2688","tase");
+	$user_name = "sqba";
+	$password = "crl2688";
+	$database = "tase";
+	$server = "localhost";
 
-// Check connection
-if (mysqli_connect_errno($con))
-  {
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-  else
-  {
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle);
+
+	if ($db_found) {
+
 	$SQL = "SELECT * FROM portfolio_status";
 	$result = mysql_query($SQL);
 
 	while ( $db_field = mysql_fetch_assoc($result) ) {
+
 		print $db_field['ticker'] . "<BR>";
 		print $db_field['buy_date'] . "<BR>";
 		print $db_field['buy_price'] . "<BR>";
@@ -21,9 +23,9 @@ if (mysqli_connect_errno($con))
 		print $db_field['last_price'] . "<BR>";
 		print $db_field['current_value'] . "<BR>";
 		print $db_field['yield'] . "<BR>";
-	}
-  }
 
-mysqli_close($con);
+	}
+
+	mysql_close($db_handle);
 ?>
 </body></html>
