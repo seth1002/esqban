9b4791a0157125ef234b4a2fc719f77400eac092
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-08-30 14:30:25 +0000

    php mysql test

--- projects/TASE/src/www/index.php
+++ projects/TASE/src/www/index.php
@@ -2,18 +2,28 @@
 <p>This is the default web page for this server.</p>
 <p>The web server software is running but no content has been added, yet.</p>
 <?php
-echo "test";
-
 $con=mysqli_connect("localhost","sqba","crl2688","tase");
 
 // Check connection
 if (mysqli_connect_errno($con))
   {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
   else
   {
-  echo "Connected";
+	$SQL = "SELECT * FROM portfolio_status";
+	$result = mysql_query($SQL);
+
+	while ( $db_field = mysql_fetch_assoc($result) ) {
+
+	print $db_field['ticker'] . "<BR>";
+	print $db_field['buy_date'] . "<BR>";
+	print $db_field['buy_price'] . "<BR>";
+	print $db_field['quantity'] . "<BR>";
+	print $db_field['buy_value'] . "<BR>";
+	print $db_field['last_price'] . "<BR>";
+	print $db_field['current_value'] . "<BR>";
+	print $db_field['yield'] . "<BR>";
   }
 
 mysqli_close($con);
