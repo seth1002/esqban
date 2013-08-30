43a5434fa2cc6f9cdfe259ce4df5d7663870f949
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-08-30 14:39:23 +0000

    php mysql test

--- projects/TASE/src/www/index.php
+++ projects/TASE/src/www/index.php
@@ -15,15 +15,15 @@ if (mysqli_connect_errno($con))
 	$result = mysql_query($SQL);
 
 	while ( $db_field = mysql_fetch_assoc($result) ) {
-
-	print $db_field['ticker'] . "<BR>";
-	print $db_field['buy_date'] . "<BR>";
-	print $db_field['buy_price'] . "<BR>";
-	print $db_field['quantity'] . "<BR>";
-	print $db_field['buy_value'] . "<BR>";
-	print $db_field['last_price'] . "<BR>";
-	print $db_field['current_value'] . "<BR>";
-	print $db_field['yield'] . "<BR>";
+		print $db_field['ticker'] . "<BR>";
+		print $db_field['buy_date'] . "<BR>";
+		print $db_field['buy_price'] . "<BR>";
+		print $db_field['quantity'] . "<BR>";
+		print $db_field['buy_value'] . "<BR>";
+		print $db_field['last_price'] . "<BR>";
+		print $db_field['current_value'] . "<BR>";
+		print $db_field['yield'] . "<BR>";
+	}
   }
 
 mysqli_close($con);
