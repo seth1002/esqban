471556e65426dd2e610c4ff3d7cda5eb7631540b
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-08-31 11:07:07 +0000

    news

--- projects/TASE/src/www/graph2.php
+++ projects/TASE/src/www/graph2.php
@@ -52,35 +52,7 @@ $(function() {
 			// the event marker flags
 			{
 				type : 'flags',
-				data : [{
-					x : Date.UTC(2011, 3, 25),
-					title : 'H',
-					text : 'Euro Contained by Channel Resistance'
-				}, {
-					x : Date.UTC(2011, 3, 28),
-					title : 'G',
-					text : 'EURUSD: Bulls Clear Path to 1.50 Figure'
-				}, {
-					x : Date.UTC(2011, 4, 4),
-					title : 'F',
-					text : 'EURUSD: Rate Decision to End Standstill'
-				}, {
-					x : Date.UTC(2011, 4, 5),
-					title : 'E',
-					text : 'EURUSD: Enter Short on Channel Break'
-				}, {
-					x : Date.UTC(2011, 4, 6),
-					title : 'D',
-					text : 'Forex: U.S. Non-Farm Payrolls Expand 244K, U.S. Dollar Rally Cut Short By Risk Appetite'
-				}, {
-					x : Date.UTC(2011, 4, 6),
-					title : 'C',
-					text : 'US Dollar: Is This the Long-Awaited Recovery or a Temporary Bounce?'
-				}, {
-					x : Date.UTC(2011, 4, 9),
-					title : 'B',
-					text : 'EURUSD: Bearish Trend Change on Tap?'
-				}],
+				data : data2,
 				onSeries : 'dataseries',
 				shape : 'circlepin',
 				width : 16
