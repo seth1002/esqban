dc85235f0a8513a676037d95abd9ba0af7c3c58b
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-08-31 11:17:33 +0000

    news

--- projects/TASE/src/www/graph2.php
+++ projects/TASE/src/www/graph2.php
@@ -13,50 +13,63 @@ $(function() {
 	var str_query1 = "json.php?symbol=" + str_symbol + "&callback=?"
 	var str_query2 = "news_json.php?symbol=" + str_symbol + "&callback=?"
 	
-	var data2;
+	
+	function create_chart(prices, news)
+	{
+		$('#container').highcharts('StockChart', {
+			
 
-	$.getJSON(str_query1, function(data_prices) {
-		$.getJSON(str_query2, function(data_news) {
-			// Create the chart
-			$('#container').highcharts('StockChart', {
-				
+			rangeSelector : {
+				selected : 1
+			},
 
-				rangeSelector : {
-					selected : 1
+			title : {
+				text : str_symbol + ' Stock Price'
+			},
+			
+			series : [{
+				name : str_symbol + ' Stock Price',
+				data : prices,
+				type : 'area',
+				threshold : null,
+				tooltip: {
+					valueDecimals: 2
 				},
-
-				title : {
-					text : str_symbol + ' Stock Price'
-				},
-				
-				series : [{
-					name : str_symbol + ' Stock Price',
-					data : data_prices,
-					type : 'area',
-					threshold : null,
-					tooltip: {
-						valueDecimals: 2
+				fillColor : {
+					linearGradient : {
+						x1: 0, 
+						y1: 0, 
+						x2: 0, 
+						y2: 1
 					},
-					fillColor : {
-						linearGradient : {
-							x1: 0, 
-							y1: 0, 
-							x2: 0, 
-							y2: 1
-						},
-						stops : [[0, Highcharts.getOptions().colors[0]], [1, 'rgba(0,0,0,0)']]
-					}
-				},
-				// the event marker flags
-				{
-					type : 'flags',
-					data : data_news,
-					onSeries : 'dataseries',
-					shape : 'circlepin',
-					width : 16
-				}]
-			});
+					stops : [[0, Highcharts.getOptions().colors[0]], [1, 'rgba(0,0,0,0)']]
+				}
+			},
+			// the event marker flags
+			{
+				type : 'flags',
+				data : news,
+				onSeries : 'dataseries',
+				shape : 'circlepin',
+				width : 16
+			}]
 		});
+	}
+
+	var data_prices="";
+	var data_news="";
+	
+	$.getJSON(str_query1, function(data) {
+		if(data_news != "")
+			create_chart(data, data_news);
+		else
+			data_prices = data;
+	});
+	$.getJSON(str_query2, function(data) {
+		if(data_prices != "")
+			create_chart(data_prices, data);
+		else
+			data_news = data;
 	});
 
 });
