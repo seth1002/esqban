d87f291fbe457ee216a28fbf003bc61fff119ba6
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-09-06 15:05:56 +0000

    full path

--- projects/TASE/src/tase/pipelines.py
+++ projects/TASE/src/tase/pipelines.py
@@ -510,6 +510,6 @@ class MyImagesPipeline(ImagesPipeline):
         image_paths = [x['path'] for ok, x in results if ok]
         if not image_paths:
             raise DropItem("Item contains no images")
-		base_path = settings.get('IMAGES_STORE')
+        base_path = settings.get('IMAGES_STORE')
         item['image_path'] = os.path.join(base_path, image_paths[0])
         return item
