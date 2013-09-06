e8372949f8f9af75648820f6bda734af5e388e27
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-09-06 21:46:22 +0000

    sectors

---
+++ projects/TASE/src/www/sectors.php
@@ -0,0 +1,34 @@
+<?php
+	header('Content-Type: application/json');
+	$user_name = "sqba";
+	$password = "crl2688";
+	$database = "tase";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle);
+
+	if ($db_found) {
+
+	$SQL = "SELECT name FROM sectors ORDER BY name ASC";
+	$result = mysql_query($SQL);
+
+	print $_GET["callback"] . "(";
+	$start = true;
+	$output = array();	
+	while ( $db_field = mysql_fetch_assoc($result) ) {
+		$output[]=$db_field;
+	}
+	print(json_encode($output));
+	print(")");
+
+	mysql_close($db_handle);
+}
+else {
+
+print "Database NOT Found ";
+mysql_close($db_handle);
+
+
+}
+?>
