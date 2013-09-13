5c8a0ca6963d95acb0767b6fb25a09d32b8fa5fb
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-09-13 18:12:44 +0000

    marinas json php

---
+++ projects/marina/php/markers.php
@@ -0,0 +1,31 @@
+<?php
+	header('Content-Type: application/json');
+	$user_name = "sqba";
+	$password = "crl2688";
+	$database = "marinas";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle);
+
+	if ($db_found) {
+		$SQL = "SELECT name, latitude, longitude FROM marinas WHERE latitude < " . $_GET["a"] . " AND latitude > " . $_GET["b"] . " AND longitude < " . $_GET["c"] . " AND longitude > " . $_GET["d"] . " LIMIT 100")
+
+		$SQL .= " ORDER BY name ASC";
+		$result = mysql_query($SQL);
+		
+		print $_GET["callback"] . "(";
+		$start = true;
+		$output = array();	
+		while ( $db_field = mysql_fetch_assoc($result) ) {
+			$output[]=$db_field;
+		}
+		print(json_encode($output));
+		print(")");
+
+		mysql_close($db_handle);
+	} else {
+		print "Database NOT Found ";
+		mysql_close($db_handle);
+	}
+?>
