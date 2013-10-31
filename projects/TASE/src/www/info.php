de4d8c394f20a552c72e9f04d9a73287ae6391e4
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-10-31 09:12:25 +0000

    info

---
+++ projects/TASE/src/www/info.php
@@ -0,0 +1,36 @@
+<?php
+	//header('Content-Type: application/json');
+	print $_GET["callback"] . "(" . "[";
+	$user_name = "sqba";
+	$password = "crl2688";
+	$database = "tase";
+	$server = "localhost";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle);
+
+	if ($db_found) {
+
+	$SQL = "SELECT url, tase_url FROM companies where symbol='" . $_GET["symbol"] . "'";
+	$result = mysql_query($SQL);
+
+	while ( $db_field = mysql_fetch_assoc($result) ) {
+		print "<a href='";
+		print $db_field['url'];
+		print "'>URL</a>";
+
+		print "<a href='";
+		print $db_field['tase_url'];
+		print "'>TASE URL</a>";
+	}
+
+	mysql_close($db_handle);
+}
+else {
+
+print "Database NOT Found ";
+mysql_close($db_handle);
+
+
+}
+?>
