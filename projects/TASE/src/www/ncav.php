7431f39e5c1f5f071a3fd3994228a4e0f3e774ac
esqban@gmail.com <esqban@gmail.com@60a7b069-80cc-7cf9-8d9f-ba4a825ddc69>
2013-11-01 10:05:59 +0000

    distinct

--- projects/TASE/src/www/ncav.php
+++ projects/TASE/src/www/ncav.php
@@ -10,7 +10,7 @@
 
 	if ($db_found) {
 
-	$SQL = "SELECT symbol as name, symbol, ncav FROM ncav ORDER BY ncav DESC";
+	$SQL = "SELECT DISTINCT symbol as name, symbol, ncav FROM ncav ORDER BY ncav DESC";
 
 	$result = mysql_query($SQL);
 	
